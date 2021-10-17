c012b371e4d014d606f56403dea25bd005fa5dfd
“ootomoakiyoshi” <ootomoakiyoshi@icloud.com>
2021-10-17 09:30:32 +0000

    ひささんに渡す

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :administrators
-  devise_for :members
-  get 'products/index'
-  get 'products/show'
-  devise_for :users
+  
+    
+# 管理者用
+# URL /admin/sign_in ...
+
+  
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+    passwords: 'admin/passwords',
+     registrations: 'admin/registrations'
+  }   
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+ end
