d0195052b33f9f9f8ca758b0ad0838afd9f7483c
kentausui0713 <usuikenta100@gmail.com>
2021-10-17 12:22:03 +0000

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :administrators
+
   devise_for :members,skip: [:passwords,], controllers: {
     registrations: "members/registrations",
     sessions: 'members/sessions'
@@ -7,6 +7,5 @@ Rails.application.routes.draw do
    
   get 'members/products/index'
   get 'products/show'
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
