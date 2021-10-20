9286dac225c158423cc928c35207d89f30243ebf
“ootomoakiyoshi” <ootomoakiyoshi@icloud.com>
2021-10-20 10:00:09 +0000

    aki

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 
 ActiveRecord::Schema.define(version: 2021_10_19_082211) do
 
-
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
