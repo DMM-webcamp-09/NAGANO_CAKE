b5ca040d3d15d7c1af9e598717a292f367ca544f
dai-dai <daiguu2021@gmail.com>
2021-10-20 10:28:19 +0000

    AddProduct

--- config/routes.rb
+++ config/routes.rb
@@ -34,22 +34,4 @@ end
 
 
 
-  namespace :member do
-    get 'products/index'
-    get 'products/show'
-  end
- devise_for :members,skip: [:passwords,], controllers: {
-  registrations: "members/registrations",
-  sessions: 'members/sessions'
-}
-
-  get 'homes/about'
-  get 'members/products/index'
-  get 'products/show'
-
-  get 'products/index'
-  get 'products/show'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
 
