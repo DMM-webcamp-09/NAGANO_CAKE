c8fb803fbd4e460eb4454afa35cb0046c64de098
sr1011 <ryo.s.1011.football@gmail.com>
2021-10-21 10:03:15 +0000

    sakurai

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,5 @@ class Order < ApplicationRecord
     belongs_to :customer
     has_many :cart_detailss, dependent: :destroy
     
-    enum payment_method: { credit_card: 0, transfer: 1 }
+    enum payment_method: { credit_card:0, transfer:1 }
 end
