65f792a863ab22bb40e131a57dbef3b74ddb87d9
dai-dai <daiguu2021@gmail.com>
2021-10-21 01:24:09 +0000

    Add2

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_10_20_024422) do
-
-
-ActiveRecord::Schema.define(version: 2021_10_19_082211) do
-
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
