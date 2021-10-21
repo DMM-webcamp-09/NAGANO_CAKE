cf385cf9c938003cccab6970e2ef02730f877a9e
sr1011 <ryo.s.1011.football@gmail.com>
2021-10-21 05:47:38 +0000

    sakurai

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -1,2 +1,16 @@
 class CartProduct < ApplicationRecord
+  
+  belongs_to :member
+  belongs_to :product
+  
+  #商品の税込価格を出すメソッド
+  def add_tax_price
+    (self.product.price * 1.10).round
+  end
+  
+  #商品毎の合計金額を出すメソッド
+  def sub_price
+      add_tax_price * self.amount
+  end
+  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,9 @@ class Product < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :cart_products, dependent: :destroy
   attachment :image
-
-
-
+  
+  def add_tax_price
+    (self.price * 1.10).round
+  end
 
 end
