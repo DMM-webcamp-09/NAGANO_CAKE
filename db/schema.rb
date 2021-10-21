c4762645c277635cbbabf6875d8efb77c11c8847
sr1011 <ryo.s.1011.football@gmail.com>
2021-10-21 01:25:21 +0000

    sakurai

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_10_19_082211) do
-
+ActiveRecord::Schema.define(version: 2021_10_20_071542) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
